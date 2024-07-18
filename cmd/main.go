6a6d1369e5e231b36f1f13b44838a1d5ec698a56
Eavan Pattie <eavan.pattie@gmail.com>
2024-07-18 21:59:19 +0200

    Create basic call to grafana

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+    "github.com/grafana/grafana-openapi-client-go/client"
+    "github.com/go-openapi/strfmt"
+    "crypto/tls"
+    "os"
+    "log"
     "fmt"
+    "strings"
+)
+
+const (
+    grafana_secret_path = "./GRAFANA_KEY"
 )
 
 func main() {
-    fmt.Println("Hello world")
+    logger := log.Default()
+
+    data, err := os.ReadFile(grafana_secret_path)
+    if err != nil {
+        logger.Fatalf("Reading secret from file produced err %s", err.Error())
+    }
+    key := strings.TrimSpace(string(data))
+
+    c := client.NewHTTPClientWithConfig(strfmt.Default, &client.TransportConfig{
+        Host: "localhost:3000",
+        BasePath: "/api",
+        Schemes: []string{"http"},
+        APIKey: key,
+        TLSConfig: &tls.Config{},
+        Debug: false,
+    })
+
+    dash, err := c.Dashboards.GetDashboardByUID("eds5h4owpwcg0b")
+    if err != nil {
+        logger.Fatalf("Unable to fetch dashboard: %s", err.Error())
+    }
+
+    fmt.Printf("Received dashboard '%s'\n", dash.Payload.Meta.Slug)
 }
